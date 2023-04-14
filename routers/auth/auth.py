2586abd075e11d0adb7de8cc9d7e40fea8b61e2a
Ahmed Maher <a7medmaher309@gmail.com>
2023-04-14 11:36:58 +0200

    add verify api

--- routers/auth/auth.py
+++ routers/auth/auth.py
@@ -1,16 +1,38 @@
 from fastapi import APIRouter
-from fastapi import FastAPI, HTTPException
-from .db_settings import get_db_conn, User, is_unique, get_password_hash, send_verification
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from .db_settings import get_db_conn, User, is_unique, get_password_hash, send_verification_email
+import jwt
+from datetime import datetime, timedelta
 
 router = APIRouter()
+
 @router.post("/signup")
 async def signup(user: User):
     db = get_db_conn()
     if not is_unique(user.email):
-        return {"message": "email is already signed"}
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="email is already registered")
     hashed_password = get_password_hash(user.password)
     user.password = hashed_password
+    user.is_verified = False # Set is_verified flag to False
     db["User"].insert_one(user.dict())
-    send_verification(user)
-    raise HTTPException(status_code=200, detail="user signed up successfully")
+    send_verification_email(user.email, jwt.encode({"email": user.email, "exp": datetime.utcnow() + timedelta(hours=1)}, "secret_key", algorithm="HS256"))
+    return JSONResponse(content={"message": "Please verify your email before logging in"}, status_code=status.HTTP_200_OK)
+
+@router.get("/verify")
+async def verify_email(token: str):
+    try:
+        payload = jwt.decode(token, "secret_key", algorithms=["HS256"])
+        email = payload["email"]
+        expiration_time = datetime.fromtimestamp(payload["exp"])
+        if datetime.utcnow() > expiration_time:
+            return JSONResponse(content={"message": "Token has expired"}, status_code=status.HTTP_400_BAD_REQUEST)
+        db = get_db_conn()
+        result = db["User"].update_one({"email": email}, {"$set": {"is_verified": True}})
+        if result.modified_count == 1:
+            return JSONResponse(content={"message": "Email verified successfully"}, status_code=status.HTTP_200_OK)
+        else:
+            return JSONResponse(content={"message": "Email not found"}, status_code=status.HTTP_404_NOT_FOUND)
+    except jwt.exceptions.DecodeError:
+        return JSONResponse(content={"message": "Invalid token"}, status_code=status.HTTP_400_BAD_REQUEST)
 
--- routers/auth/db_settings.py
+++ routers/auth/db_settings.py
@@ -4,6 +4,9 @@ from passlib.context import CryptContext
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import jwt
+from datetime import datetime, timedelta
+
 
 
 SECRET_KEY = "785c70b3a4426c5bf3b623a6a7011fc4ef04a602f35c0f57852814fe988af8b3"
@@ -16,6 +19,7 @@ class User(BaseModel):
     lastname: str
     email: str
     password: str
+    is_verified: bool = False
 
 def get_db_conn():
     client = MongoClient('mongodb://localhost:27017')
@@ -37,27 +41,34 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def send_verification_email(email: str, verification_link: str):
+def send_verification_email(email: str, token: str):
     # Create a message object
     message = MIMEMultipart()
     message["From"] = "a7medmaher309@gmail.com"
     message["To"] = email
     message["Subject"] = "Verify your email address"
-
+    # Calculate token expiration date (e.g. 1 hour)
+    expiration_date = datetime.utcnow() + timedelta(hours=1)
+    # Create the verification link with the token
+    verification_link = f"https://example.com/verify?token={token}"
     # Create the HTML content of the email
-    html = f"<p>Thank you for signing up! Please click the following link to verify your email address:</p><p><a href='{verification_link}'>{verification_link}</a></p>"
-
+    html = f"<p>Thank you for signing up! Please click the following link to verify your email address:</p><p><a href='{verification_link}'>{verification_link}</a></p><p>The link will expire on {expiration_date}.</p>"
     # Attach the HTML content to the message
     message.attach(MIMEText(html, "html"))
-
     # Create a connection to the SMTP server
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
         server.login("a7medmaher309@gmail.com", "gsrbcwieakzdxyvk")
-
         # Send the message
         server.sendmail("your_email@example.com", email, message.as_string())
+
+
 def send_verification(user: User):
-    verification_link = f"https://example.com/verify?email={user.email}"
-    send_verification_email(user.email, verification_link)
+    # Create the authentication token with an expiration date
+    expiration_date = datetime.utcnow() + timedelta(hours=1)
+    payload = {"email": user.email, "exp": expiration_date}
+    token = jwt.encode(payload, "secret_key", algorithm="HS256")
+    # Send the verification email with the token
+    send_verification_email(user.email, token)
+
 
