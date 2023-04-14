052a146be5eee707bd4a09056677f97e4cb53d76
Ammar Al-Morsi <ammaralmorsi@gmail.com>
2023-04-14 20:10:13 +0200

    Integrate tickets APIs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from routers.events import events
 from routers.categories import categories
+from routers.tickets import tickets
 
 app = FastAPI(
     title="EventBrite",
@@ -11,3 +12,4 @@ app = FastAPI(
 
 app.include_router(categories.router)
 app.include_router(events.router)
+app.include_router(tickets.router)
