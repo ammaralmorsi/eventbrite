a9834f63be5b1538ee923bae0a705c101bff2d63
Ahmed Maher <a7medmaher309@gmail.com>
2023-04-19 01:22:20 +0200

    refactor commons.py

--- routers/auth/auth.py
+++ routers/auth/auth.py
@@ -9,14 +9,12 @@ from starlette.responses import RedirectResponse
 import jwt
 
 from .commons import is_unique
-from .commons import get_password_hash
-from .commons import verify_password
-from .commons import send_email
-from .commons import encode_token
-from .commons import decode_token
 from .db.models import User, LoginUser, ChangePasswordRequest
 from .db.driver import get_db_conn
-
+from .password_handler import PasswordHandler
+from .token_handler import TokenHandler
+from .email_handler import EmailHandler
+from .email_handler import EmailType
 
 from datetime import datetime
 
@@ -25,26 +23,20 @@ router = APIRouter(
     tags=["auth"]
 )
 
+password_handler = PasswordHandler()
+token_handler = TokenHandler()
+email_handler = EmailHandler()
+db = get_db_conn()
 
 @router.post("/signup")
 async def signup(user: User):
-    """
-    Register a new user with the given user object and sends a verification email.
-
-    Args:
-        user (User): A user object representing the user to register.
-
-    Returns:
-        JSONResponse: A JSON response containing a success message or an error message if the email is already registered.
-    """
-    db = get_db_conn()
     if not is_unique(user.email):
         return JSONResponse(content={"message": "email is already registered"}, status_code=status.HTTP_400_BAD_REQUEST)
-    token = encode_token(user.email)
-    send_email_value, body = send_email(user.email, token, 1)
+    token = token_handler.encode_token(user.email)
+    send_email_value, body = email_handler.send_email(user.email, token, EmailType.SIGNUP_VERIFICATION)
     if send_email_value == -1:
         return JSONResponse(content={"message": body}, status_code=status.HTTP_400_BAD_REQUEST)
-    hashed_password = get_password_hash(user.password)
+    hashed_password = password_handler.get_password_hash(user.password)
     user.password = hashed_password
     db["User"].insert_one(user.dict())
     return JSONResponse(content={"message": "Please verify your email before your login"},
@@ -52,20 +44,10 @@ async def signup(user: User):
 
 @router.get("/verify")
 async def verify_email(token: str):
-    """
-    Verify a user's email address with the given verification token.
-
-    Args:
-        token (str): A string containing the verification token to use.
-
-    Returns:
-        JSONResponse: A JSON response containing a success message if the email is verified successfully, or an error message if the token is invalid or has expired.
-    """
     try:
-        email, expiration_time = decode_token(token)
+        email, expiration_time = token_handler.decode_token(token)
         if datetime.utcnow() > expiration_time:
             return JSONResponse(content={"message": "Token has expired"}, status_code=status.HTTP_400_BAD_REQUEST)
-        db = get_db_conn()
         result = db["User"].update_one({"email": email}, {"$set": {"is_verified": True}})
         if result.modified_count == 1:
             return JSONResponse(content={"message": "Email verified successfully"}, status_code=status.HTTP_200_OK)
@@ -77,28 +59,15 @@ async def verify_email(token: str):
 
 @router.post("/login")
 async def login(user: LoginUser):
-    """
-    Authenticate a user with the given login credentials and return an access token.
-
-    Args:
-        user (LoginUser): A login user object containing the email and password to authenticate.
-
-    Returns:
-        JSONResponse: A JSON response containing an access token or an error message if the credentials are invalid or the email is not verified.
-    """
-    if user.email == "" or user.password == "":
-        return JSONResponse(content={"message": "please, provide both email and password"},
-                            status_code=status.HTTP_400_BAD_REQUEST)
-    db = get_db_conn()
     logged_user = db["User"].find_one({"email": user.email})
     if not logged_user:
         return JSONResponse(content={"message": "email is not registered"}, status_code=status.HTTP_401_UNAUTHORIZED)
 
-    if not verify_password(user.password, logged_user["password"]):
+    if not password_handler.verify_password(user.password, logged_user["password"]):
         return JSONResponse(content={"message": "wrong password"}, status_code=status.HTTP_401_UNAUTHORIZED)
-    encoded_token = encode_token(user.email)
+    encoded_token = token_handler.encode_token(user.email)
     if not logged_user["is_verified"]:
-        send_email(logged_user["email"], encoded_token, 1)
+        email_handler.send_email(logged_user["email"], encoded_token, EmailType.SIGNUP_VERIFICATION)
         return JSONResponse(content={"message": "email is not verified"}, status_code=status.HTTP_401_UNAUTHORIZED)
     return JSONResponse(content={"token": encoded_token, "email": logged_user["email"],
                                  "firstname": logged_user["firstname"], "lastname": logged_user["lastname"],
@@ -107,42 +76,20 @@ async def login(user: LoginUser):
 
 @router.get("/forgot-password")
 async def forgot_password(email):
-    """
-    Sends a forgot password email to the given email address.
-
-    Args:
-        email (str): A string containing the email address to send the forgot password email to.
-
-    Returns:
-        JSONResponse: A JSON response containing a success message or an error message if the email is not found.
-    """
-    if email == "":
-        return JSONResponse(content={"message": "please, provide both email and password"}, status_code=status.HTTP_400_BAD_REQUEST)
-    db = get_db_conn()
     logged_user = db["User"].find_one({"email": email})
     if not logged_user:
         return JSONResponse(content={"message": "Email is not found"}, status_code=status.HTTP_404_NOT_FOUND)
-    encoded_token = encode_token(email)
-    send_email(email, encoded_token, 2)
+    encoded_token = token_handler.encode_token(email)
+    email_handler.send_email(email, encoded_token, EmailType.FORGET_PASSWORD)
     return JSONResponse(content={"message": "Sent a verification email"}, status_code=status.HTTP_200_OK)
 
 
 @router.get("/reset-password")
 async def reset_password(token: str):
-    """
-    Verify a user's reset password token and redirect to the change password page.
-
-    Args:
-        token (str): A string containing the reset password token to use.
-
-    Returns:
-        RedirectResponse or JSONResponse: A redirect response to the change password page if the token is valid, or a JSON response containing an error message if the token is invalid or has expired.
-    """
     try:
-        email, expiration_time = decode_token(token)
+        email, expiration_time = token_handler.decode_token(token)
         if datetime.utcnow() > expiration_time:
             return JSONResponse(content={"message": "Token has expired"}, status_code=status.HTTP_400_BAD_REQUEST)
-        db = get_db_conn()
         logged_user = db["User"].find_one({"email": email})
         if not logged_user:
             return JSONResponse(content={"message": "Email not found"}, status_code=status.HTTP_404_NOT_FOUND)
@@ -154,28 +101,15 @@ async def reset_password(token: str):
 
 @router.put("/change-password")
 async def change_password(token: str, request: ChangePasswordRequest):
-    """
-    Update a user's password with the given reset password token and new password.
-
-    Args:
-        token (str): A string containing the reset password token to use.
-        request (ChangePasswordRequest): A change password request object containing the new password to use.
-
-    Returns:
-        JSONResponse: A JSON response containing a success message or an error message if the token is invalid or has expired.
-    """
     try:
-        email, expiration_time = decode_token(token)
+        email, expiration_time = token_handler.decode_token(token)
         if datetime.utcnow() > expiration_time:
             return JSONResponse(content={"message": "Token has expired"}, status_code=status.HTTP_400_BAD_REQUEST)
-        db = get_db_conn()
         logged_user = db["User"].find_one({"email": email})
         if not logged_user:
             return JSONResponse(content={"message": "Email not found"}, status_code=status.HTTP_404_NOT_FOUND)
-        # update user password in database
-        new_password = get_password_hash(request.new_password)
+        new_password = password_handler.get_password_hash(request.new_password)
         db["User"].update_one({"email": email}, {"$set": {"password": new_password}})
-        # return success message
         return JSONResponse(content={"message": "Password updated successfully"}, status_code=status.HTTP_200_OK)
     except jwt.exceptions.DecodeError:
         return JSONResponse(content={"message": "Invalid token"}, status_code=status.HTTP_400_BAD_REQUEST)
@@ -183,7 +117,6 @@ async def change_password(token: str, request: ChangePasswordRequest):
 
 @router.post("/check-email")
 async def check_email(email):
-    db = get_db_conn()
     logged_user = db["User"].find_one({"email": email})
     if not logged_user:
         return False
--- routers/auth/db/models.py
+++ routers/auth/db/models.py
@@ -1,52 +1,17 @@
 from pydantic import BaseModel
 
 class User(BaseModel):
-    """
-    Represents a user.
-
-    Attributes:
-    -----------
-    firstname : str
-        The user's first name.
-    lastname : str
-        The user's last name.
-    email : str
-        The user's email address.
-    password : str
-        The user's password.
-    is_verified : bool, optional
-        Whether the user's email address has been verified. Defaults to False.
-    """
     firstname: str
     lastname: str
     email: str
     password: str
     is_verified: bool = False
-    avatar_url:str
+    avatar_url: str
 
 class LoginUser(BaseModel):
-    """
-    Represents a user attempting to log in.
-
-    Attributes:
-    -----------
-    email : str
-        The user's email address.
-    password : str
-        The user's password.
-    """
     email: str
     password: str
 
 
-
 class ChangePasswordRequest(BaseModel):
-    """
-    Represents a request to change a user's password.
-
-    Attributes:
-    -----------
-    new_password : str
-        The user's new password.
-    """
     new_password: str
