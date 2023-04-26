35ae00f1611bfa911f2d1c8a0a805515914d58dd
Abdulhameed Osama <meedosama2001@gmail.com>
2023-04-26 12:41:58 +0200

    Refactored Tickets

--- routers/tickets/db/driver.py
+++ routers/tickets/db/driver.py
@@ -2,61 +2,24 @@ import os
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+
 class TicketDriver:
     """
     A class for handling CRUD operations related to tickets in a MongoDB database.
-
     """
     def __init__(self):
         """
         Initializes a new instance of the TicketDriver class.
-
         """
         self.client = MongoClient(os.environ.get("MONGO_URI"))
         self.db = self.client[os.environ.get("MONGO_DB")]
         self.collection = self.db['events']
 
-    def insert(self, event_id, tickets):
-        """
-        Inserts a document into the MongoDB collection.
+    def find_by_event_id(self, event_id):
+        return self.collection.find_one({"_id": ObjectId(event_id)})
 
-        Args:
-            event_id: The ID of the event to create tickets for.
-            tickets: A list of tickets to be created.
-
-        Returns:
-            The result of the insertion operation.
-
-        """
-        # event = self.collection.find_one({"_id": event_id})
-
-        return self.collection.update_one({"_id": event_id}, {"$set": {"tickets": tickets}})
-
-    def find_by_event_id(self, query):
-        """
-        Finds a document in the MongoDB collection by a given query.
-
-        Args:
-            query: A dictionary containing the query criteria.
-
-        Returns:
-            The document that matches the query criteria, or None if no match is found.
-
-        """
-        return self.collection.find_one(query)
-
-    def count(self, query):
-        """
-        Counts the number of documents in the MongoDB collection that match a given query.
-
-        Args:
-            query: A dictionary containing the query criteria.
-
-        Returns:
-            The number of documents that match the query criteria.
-
-        """
-        return self.collection.count_documents(query)
+    def is_valid_event_id(self, event_id):
+        return self.collection.count_documents({"_id": ObjectId(event_id)})
 
     def update_tickets(self, event_id, tickets):
         return self.collection.update_one({"_id": ObjectId(event_id)}, {"$set": {"tickets": tickets}})
--- routers/tickets/tickets.py
+++ routers/tickets/tickets.py
@@ -1,10 +1,8 @@
 from typing import List
-from bson import ObjectId
 from fastapi import APIRouter
 from fastapi.responses import PlainTextResponse
 from .db.driver import TicketDriver
 from ..events.db.embeded_models.ticket import Ticket
-
 
 router = APIRouter(
     prefix="/tickets",
@@ -12,6 +10,19 @@ router = APIRouter(
 )
 
 db_handler = TicketDriver()
+
+
+def is_valid_event_id(event_id):
+    return db_handler.is_valid_event_id(event_id)
+
+
+def is_valid_update(event_id, tickets):
+    if db_handler.update_tickets(event_id, tickets):
+        if not tickets:
+            return PlainTextResponse("Tickets deleted successfully", status_code=200)
+        return PlainTextResponse("Tickets updated successfully", status_code=200)
+    else:
+        return PlainTextResponse("Tickets update failed", status_code=500)
 
 
 @router.post(
@@ -24,7 +35,7 @@ db_handler = TicketDriver()
     },
 )
 async def create_tickets_by_event_id(event_id: str, tickets: List[Ticket]):
-    tickets_in_event = db_handler.find_by_event_id({"_id": ObjectId(event_id)})
+    tickets_in_event = db_handler.find_by_event_id(event_id)
     result = []
 
     tickets_as_dicts = [ticket.dict() for ticket in tickets]
@@ -34,10 +45,7 @@ async def create_tickets_by_event_id(event_id: str, tickets: List[Ticket]):
     result.extend(tickets_in_event["tickets"])
     result.extend(tickets_as_dicts)
 
-    if db_handler.update_tickets(event_id, result):
-        return PlainTextResponse("Tickets created successfully", status_code=200)
-    else:
-        return PlainTextResponse("Tickets creation failed", status_code=500)
+    return is_valid_update(event_id, result)
 
 
 @router.get(
@@ -51,12 +59,10 @@ async def create_tickets_by_event_id(event_id: str, tickets: List[Ticket]):
 )
 async def get_tickets_by_event_id(event_id: str) -> List[Ticket]:
 
-
-    if db_handler.count({"_id": ObjectId(event_id)}) == 0:
+    if is_valid_event_id(event_id) == 0:
         return []
 
-
-    tickets = db_handler.find_by_event_id({"_id": ObjectId(event_id)})
+    tickets = db_handler.find_by_event_id(event_id)
 
     result = []
     tickets = tickets["tickets"]
@@ -77,43 +83,14 @@ async def get_tickets_by_event_id(event_id: str) -> List[Ticket]:
 )
 async def update_tickets_by_event_id(event_id: str, tickets: List[Ticket]):
 
-
-    if db_handler.count({"_id": ObjectId(event_id)}) == 0:
+    if is_valid_event_id(event_id) == 0:
         return PlainTextResponse("Event not found", status_code=404)
 
     tickets_as_dicts = [ticket.dict() for ticket in tickets]
     for ticket in tickets_as_dicts:
         ticket["type"] = ticket["type"].value
 
-
-    db_handler.update_tickets({"_id": ObjectId(event_id)}, {"$set ": {"tickets": tickets_as_dicts}})
-    return PlainTextResponse("Tickets updated successfully", status_code=200)
-
-
-@router.get(
-    "/{event_id}/type/{ticket_type}",
-    summary="Get tickets by event id and ticket type",
-    description="This endpoint allows you to get tickets by event id and ticket type.",
-    tags=["tickets"],
-    responses={
-        200: {"description": "Tickets retrieved successfully"},
-    },
-)
-async def get_tickets_by_event_id_and_ticket_type(event_id: str, ticket_type: type) -> List[Ticket]:
-
-
-    if db_handler.count({"_id": ObjectId(event_id)}) == 0:
-        return []
-
-
-    tickets = db_handler.find_by_event_id_and_ticket_type({"_id": ObjectId(event_id)}, ticket_type)
-
-    result = []
-    tickets = tickets["tickets"]
-    for ticket in tickets:
-        ticket_out = Ticket(**ticket)
-        result.append(ticket_out)
-    return result
+    return is_valid_update(event_id, tickets_as_dicts)
 
 
 @router.delete(
@@ -127,12 +104,7 @@ async def get_tickets_by_event_id_and_ticket_type(event_id: str, ticket_type: ty
 )
 async def delete_tickets_by_event_id(event_id: str):
 
-
-    if db_handler.count({"_id": ObjectId(event_id)}) == 0:
+    if is_valid_event_id(event_id) == 0:
         return PlainTextResponse("Event not found", status_code=404)
 
-
-
-    db_handler.update_tickets({"_id": ObjectId(event_id)}, {"$set": {"tickets": []}})
-
-    return PlainTextResponse("Tickets deleted successfully", status_code=200)
+    return is_valid_update(event_id, [])
