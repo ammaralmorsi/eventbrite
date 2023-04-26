10e64fb9d859a80a2027cf60ab8ca470fc80855d
Abdulhameed Osama <meedosama2001@gmail.com>
2023-04-26 13:44:16 +0200

    Added apis to Promocodes to put, get and delete a promocode by name.

--- routers/promocodes/promocodes.py
+++ routers/promocodes/promocodes.py
@@ -19,11 +19,16 @@ def is_valid_event_id(event_id):
 
 def is_valid_update(event_id, codes):
     if db_handler.update_promocodes(event_id, codes):
-        if not codes:
-            return PlainTextResponse("Tickets deleted successfully", status_code=200)
-        return PlainTextResponse("Tickets updated successfully", status_code=200)
+        return PlainTextResponse("Promocodes updated successfully", status_code=200)
     else:
-        return PlainTextResponse("Tickets update failed", status_code=500)
+        return PlainTextResponse("Promocodes update failed", status_code=500)
+
+
+def is_valid_deletion(event_id, codes):
+    if db_handler.update_promocodes(event_id, codes):
+        return PlainTextResponse("Promocodes deleted successfully", status_code=200)
+    else:
+        return PlainTextResponse("Promocodes deletion failed", status_code=500)
 
 
 @router.post(
@@ -49,6 +54,45 @@ async def create_promocodes_by_event_id(event_id: str, promocodes: List[PromoCod
     return is_valid_update(event_id, result)
 
 
+@router.put(
+    "/{event_id}",
+    summary="Update promocodes by event id",
+    description="This endpoint allows you to update promocodes by event id.",
+    tags=["promocodes"],
+    responses={
+        200: {"description": "Promocodes updated successfully"},
+    },
+)
+async def update_promocodes_by_event_id(event_id: str, promocodes: List[PromoCode]):
+    if not is_valid_event_id(event_id):
+        return PlainTextResponse("Event ID is invalid", status_code=404)
+
+    codes_as_dicts = [promocode.dict() for promocode in promocodes]
+
+    return is_valid_update(event_id, codes_as_dicts)
+
+
+@router.put(
+    "/{event_id}/{name}",
+    summary="Update promocode by name",
+    description="This endpoint allows you to update promocode by name.",
+    tags=["promocodes"],
+    responses={
+        200: {"description": "Promocode updated successfully"},
+    },
+)
+async def update_promocode_by_event_id_and_name(event_id: str, name: str, promocode: PromoCode):
+    if not is_valid_event_id(event_id):
+        return PlainTextResponse("Event ID is invalid", status_code=404)
+
+    codes = db_handler.find_by_event_id(event_id)
+    codes = codes["promo_codes"]
+    codes = [code for code in codes if code["name"] != name]
+    codes.append(promocode.dict())
+
+    return is_valid_update(event_id, codes)
+
+
 @router.get(
     "/{event_id}",
     summary="Get promocodes by event id",
@@ -72,6 +116,28 @@ async def get_promocodes_by_event_id(event_id: str) -> List[PromoCode]:
     return result
 
 
+@router.get(
+    "/{event_id}/{name}",
+    summary="Get promocode by name",
+    description="This endpoint allows you to get promocode by name.",
+    tags=["promocodes"],
+    responses={
+        200: {"description": "Promocode retrieved successfully"},
+    },
+)
+async def get_promocode_by_event_id_and_name(event_id: str, name: str):
+    if not is_valid_event_id(event_id):
+        return PlainTextResponse("Event ID is invalid", status_code=404)
+
+    codes = db_handler.find_by_event_id(event_id)
+    codes = codes["promo_codes"]
+    for code in codes:
+        if code["name"] == name:
+            code_out = PromoCode(**code)
+            return code_out
+    return PlainTextResponse("Promocode not found", status_code=404)
+
+
 @router.delete(
     "/{event_id}",
     summary="Delete promocodes by event id",
@@ -86,3 +152,30 @@ async def delete_promocodes_by_event_id(event_id: str):
         return PlainTextResponse("Event ID is invalid", status_code=404)
 
     return is_valid_update(event_id, [])
+
+
+@router.delete(
+    "/{event_id}/{name}",
+    summary="Delete promocode by name",
+    description="This endpoint allows you to delete promocode by name.",
+    tags=["promocodes"],
+    responses={
+        200: {"description": "Promocode deleted successfully"},
+    },
+)
+async def delete_promocode_by_event_id_and_name(event_id: str, name: str):
+    if not is_valid_event_id(event_id):
+        return PlainTextResponse("Event ID is invalid", status_code=404)
+
+    promocodes_in_event = db_handler.find_by_event_id(event_id)
+
+    codes = []
+    codes.extend(promocodes_in_event["promo_codes"])
+
+    for code in codes:
+        if code["name"] == name:
+            codes.remove(code)
+            break
+    return is_valid_deletion(event_id, codes)
+
+
