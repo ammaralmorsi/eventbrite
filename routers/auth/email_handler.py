3bfbd7c875f39df12be71e7a8f0a30f58b414254
Ahmed Maher <a7medmaher309@gmail.com>
2023-05-13 20:31:25 +0300

    changed forgot password email

--- routers/auth/email_handler.py
+++ routers/auth/email_handler.py
@@ -52,7 +52,7 @@ class EmailHandler:
         return html
 
     def generate_html_for_forgot_password(self, token):
-        verification_link = f"{self.host}/change-password?token={token}"
+        verification_link = f"{self.host}/login/resetpassword?token={token}"
         html = f"<p>Click the following link to reset your password:</p>" \
                f"<p><a href='{verification_link}'>{verification_link}</a></p>" \
                f"<p>The link will expire on {self.expiration_date}.</p>"
